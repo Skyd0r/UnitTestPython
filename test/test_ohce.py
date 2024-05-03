b5a203bf2cd1343527097013a9358c2cc720b97d
Skyd0r <romain.poitrenaud@gmail..com>
2024-05-03 13:45:17 +0200

    [RED] test palindrome + "bien dit !"

--- test/test_ohce.py
+++ test/test_ohce.py
@@ -17,15 +17,24 @@ class PalindromeTest(unittest.TestCase):
         cas = ["epsi", "romain", get_random_string(10), get_random_string(100)]
 
         for chaîne in cas:
+            with self.subTest(chaîne):
+                # QUAND on vérifie si c'est un palindrome
+                résultat = VérificateurPalindrome.vérifier(chaîne)
 
-            # QUAND on vérifie si c'est un palindrome
-            résultat = VérificateurPalindrome.vérifier(chaîne)
+                # ALORS la chaîne est renvoyée en miroir
+                attendu = chaîne[::-1]
+                self.assertEqual(attendu, résultat)
 
-            # ALORS la chaîne est renvoyée en miroir
-            attendu = chaîne[::-1]
-            self.assertEqual(attendu, résultat)
+    def test_BienDit(self):
+        # ETANT DONNE un palindrome
+        palindrome = "kayak"
 
+        # QUAND on saisit un palindrome
+        résultat = VérificateurPalindrome.vérifier(palindrome)
 
+        # ALORS celui - ci est renvoyé ET « Bien dit » est envoyé ensuite
+        attendu = palindrome + "\nBien dit !"
+        self.assertEqual(attendu, résultat)
 
 if __name__ == '__main__':
     unittest.main()
