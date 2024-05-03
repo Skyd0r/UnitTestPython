26ed8cde3887db998b59a9ed4d8cbc32e311e7f1
Skyd0r <romain.poitrenaud@gmail..com>
2024-05-03 13:15:50 +0200

    [RED] test avec deux nouvelles chaines random (random testing)

--- test/test_ohce.py
+++ test/test_ohce.py
@@ -1,11 +1,20 @@
 import unittest
+import random
+import string
 
 from verificateur_palindrome import VérificateurPalindrome
 
+
+def get_random_string(length):
+    # choose from all lowercase letter
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return(result_str)
 class PalindromeTest(unittest.TestCase):
+
     def test_miroir(self):
         # ETANT DONNE un non-palindrome
-        cas = ["epsi", "romain"]
+        cas = ["epsi", "romain", get_random_string(10), get_random_string(100)]
 
         for chaîne in cas:
 
