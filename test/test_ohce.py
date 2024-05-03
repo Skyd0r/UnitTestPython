f784566064ebd0a28f6f9e0539639af2ec9b7f52
Skyd0r <romain.poitrenaud@gmail..com>
2024-05-03 11:18:48 +0200

    [RED] Premier test de mise en miroir

---
+++ test/test_ohce.py
@@ -0,0 +1,16 @@
+import unittest
+
+class PalindromeTest(unittest.TestCase):
+    def test_miroir(self):
+        # ETANT DONNE un non-palindrome
+        chaîne = "epsi"
+
+        # QUAND on vérifie si c'est un palindrome
+        résultat = VérificateurPalindrome.vérifier(chaîne)
+
+        # ALORS la chaîne est renvoyée en miroir
+        self.assertEqual("ispe", résultat)
+
+
+if __name__ == '__main__':
+    unittest.main()
